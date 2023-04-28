06734115f8b21c8b8b9bf2ed2433a4d072b2f247
Vsevolod Subbotkin <vsevolod.98t@mail.ru>
2023-04-28 14:28:01 +0300

    wole repositor

---
+++ src/aso_lab5/Main.java
@@ -0,0 +1,14 @@
+package aso_lab5;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 0;
+        
+        Philosopher[] philosophers = new Philosopher[number];
+        
+        for (int i=0; i<number; i++)
+            philosophers[i] = new Philosopher();
+        for (int i=0; i<number; i++)
+            philosophers[i].start();
+    }   
+}
---
+++ src/aso_lab5/Philosopher.java
@@ -0,0 +1,14 @@
+package aso_lab5;
+
+public class Philosopher extends Thread {
+    boolean isConsuming;
+    
+    Philosopher() {
+        this.isConsuming = false;
+    }
+    
+    @Override
+    public void run() {
+        
+    }
+}
