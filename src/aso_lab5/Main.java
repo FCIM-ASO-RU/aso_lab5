5c89a0e739fceb31b4c3a058482d986717c57f7b
Ivan <kyurkchyu10@gmail.com>
2023-05-12 16:40:35 +0300

    Main.java implementation

--- src/aso_lab5/Main.java
+++ src/aso_lab5/Main.java
@@ -2,13 +2,17 @@ package aso_lab5;
 
 public class Main {
     public static void main(String[] args) {
-        int number = 0;
-        
-        Philosopher[] philosophers = new Philosopher[number];
-        
-        for (int i=0; i<number; i++)
-            philosophers[i] = new Philosopher();
-        for (int i=0; i<number; i++)
+        int n = 6;
+        Philosopher[] philosophers = new Philosopher[n];
+        Fork[] forks = new Fork[n];
+
+        for (int i = 0; i < n; i++) {
+            forks[i] = new Fork();
+        }
+
+        for (int i = 0; i < n; i++) {
+            philosophers[i] = new Philosopher(i, forks[i], forks[(i + 1) % n]);
             philosophers[i].start();
+        }
     }   
 }
